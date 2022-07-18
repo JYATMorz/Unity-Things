eece0c05de66f13ba70f3578c5da52f23299ceba
SURFACE-R3QU3C7 <60472956+JYATMorz@users.noreply.github.com>
2022-07-18 15:42:41 +0800

    Update Player.cs

--- Unity Projects/PlatformShooting/Assets/Scripts/Player.cs
+++ Unity Projects/PlatformShooting/Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody playerBody;
+    public float jumpAcc = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Key Pressed");
+            playerBody.AddForce(Vector3.up * jumpAcc, ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate() {
         
     }
 }
