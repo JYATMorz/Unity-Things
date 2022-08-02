a1ffc5cb15f6b0e65e3d7c3ca82941baf17ab3f8
SURFACE-R3QU3C7 <60472956+JYATMorz@users.noreply.github.com>
2022-08-02 16:29:29 +0800

    Merge Enemy & Player script; Allow player reborn

--- Unity Projects/PlatformShooting/Assets/Scripts/Others/MainCamera.cs
+++ Unity Projects/PlatformShooting/Assets/Scripts/Others/MainCamera.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-// attach it to main camera, and move camera to outside
 public class MainCamera : MonoBehaviour
 {
     public Transform player;
 
     private Vector3 _cameraOffset = new(0, 1, -8);
     private string _playerTag;
+    private bool _onPlayer = true;
 
     void Awake()
     {
@@ -15,23 +15,39 @@ public class MainCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        if (player != null && !player.CompareTag("Dead"))
+        if (_onPlayer)
         {
-            // TODO: Shaking Camera, please only used during switching player
-            // transform.position = Vector3.MoveTowards(transform.position, player.position + _cameraOffset, 10 * Time.deltaTime);
+            if (player == null || player.CompareTag("Dead"))
+            {
+                _onPlayer = false;
+                ChangePlayer();
+                return;
+            }
 
             transform.position = player.position + _cameraOffset;
-            transform.LookAt(player);
         } else
         {
-            GameObject nextPlayer = GameObject.FindWithTag(_playerTag);
-            if (nextPlayer == null)
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, player.position + _cameraOffset, 5 * Time.deltaTime);
+            transform.position = newPosition;
+
+            if ((newPosition - player.position - _cameraOffset).sqrMagnitude < 0.1f)
             {
-                Debug.Log("Did Enemy team win?");
-            } else {
-                player = nextPlayer.transform;
-                // TODO: implement method to control the selected character
+                _onPlayer = true;
+                player.SendMessage("BecomePlayer");
             }
+        }
+        transform.LookAt(player);
+    }
+
+    private void ChangePlayer()
+    {
+        GameObject nextPlayer = GameObject.FindWithTag(_playerTag);
+
+        if (nextPlayer == null)
+        {
+            Debug.Log("Game Over. Did Enemy team win?");
+        } else {
+            player = nextPlayer.transform;
         }
     }
 }
